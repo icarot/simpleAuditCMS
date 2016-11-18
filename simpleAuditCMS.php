cf486d8573b49773812fb05926cedc75771b3f2d
Icaro Torres <icaro.redes.ifpb@gmail.com>
2016-11-18 00:45:17 -0200

    Update simpleAuditCMS.php
    Added the verification of the permission of the .htacess and wp-config.php files.

--- simpleAuditCMS.php
+++ simpleAuditCMS.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Simple Audit CMS: this script is intended to verify if same security configuration are applied on the CMS.
 
@@ -8,12 +8,12 @@ Simple Audit CMS: this script is intended to verify if same security configurati
 #### CORE ####:
 
 update 05-June-2016:
-- added the function in Javascript to print the results in the printer device;
+- added the function to print the results in the printer device;
 ##################
 
 #### Wordpress ####:
 
-update 12-Marc-2016: 
+update 12-Marc-2016:
 - added the verification of readme.html file and it permission;
 - added the index.html file verification in the administration directories of the Wordpress;
 
@@ -41,7 +41,7 @@ update 23-Marc-2016:
 
 update 25-Marc-2016:
 - Created a class with a method to verify a file permission based in the security recommendation.
-- Created a method in a class to verify some kind of file on a directory. 
+- Created a method in a class to verify some kind of file on a directory.
 - Created a method in a class to list files in a directory. Cleaning up the code, avoiding to repeat the same code in similar functions that test different files and directories;
 
 update 27-Marc-2016:
@@ -54,6 +54,9 @@ update 12-April-2016:
 
 update 23-April-2016:
 - added index.html verification in more files;
+
+update 17-Nov-2016:
+- added the verification of the permission of the .htacess and wp-config.php files;
 ##################
 
 #### JOOMLA ####:
@@ -86,61 +89,61 @@ $CORRECTPERM = "0600";
 //##############
 class CheckDirFile {
 //Verify if the passed file has the correct permission.
-	public function CheckPerm($correctperm, $dirpath){
-		if(file_exists($dirpath)){
-			echo "The file $dirpath exists. ";
-			$currentperm = substr(sprintf('%o', fileperms($dirpath)), -4);
-				if($currentperm !== $correctperm){
-					echo "The permission is not correct, $currentperm. Please, put the 0600 permission. (OFF) <br/>";
-				} else {
-					echo "The permission is correct, $currentperm. (OK) <br/>";
-				}
-		} else {
-			echo "The file $dirpath doesn't exists (OK) <br/>";
-			}
-	}
+        public function CheckPerm($correctperm, $dirpath){
+                if(file_exists($dirpath)){
+                        echo "The file $dirpath exists. ";
+                        $currentperm = substr(sprintf('%o', fileperms($dirpath)), -4);
+                                if($currentperm !== $correctperm){
+                                        echo "The permission is not correct, $currentperm. Please, put the $correctperm permission. (OFF) <br/>";
+                                } else {
+                                        echo "The permission is correct, $currentperm. (OK) <br/>";
+                                }
+                } else {
+                        echo "The file $dirpath doesn't exists (OK) <br/>";
+                        }
+        }
 
-//Verify some file type in a directory.	
-	public function SearchTypeFileonDir ($typefile, $dirpath){
-		if(file_exists($dirpath)){
-		$filelistdir = scandir("$dirpath");
+//Verify some file type in a directory.
+        public function SearchTypeFileonDir ($typefile, $dirpath){
+                if(file_exists($dirpath)){
+                $filelistdir = scandir("$dirpath");
 
-		echo "<br/> <b> Exists " . strtoupper($typefile) . " file in the directory ($dirpath)? </b> <br/>";
-		$negativeanswer = 0;
-		foreach( $filelistdir as $files ){
-			if(!is_dir($files) and $files != "." and $files != ".."){
-			$ext = pathinfo($files, PATHINFO_EXTENSION);
-			if($ext == $typefile){
-				echo "Please, verify this " . strtoupper($typefile) . " file in the diretory: " . $files . " (OFF) <br/>";
-				$negativeanswer++;
-			}
-			}
-		}
-			if ($negativeanswer == 0){
-				echo "No! (OK) <br/>";
-			}
-		} else {
-			echo "The file $dirpath doesn't exists (OK) <br/>";
-		}
-	}
+                echo "<br/> <b> Exists " . strtoupper($typefile) . " file in the directory ($dirpath)? </b> <br/>";
+                $negativeanswer = 0;
+                foreach( $filelistdir as $files ){
+                        if(!is_dir($files) and $files != "." and $files != ".."){
+                        $ext = pathinfo($files, PATHINFO_EXTENSION);
+                        if($ext == $typefile){
+                                echo "Please, verify this " . strtoupper($typefile) . " file in the diretory: " . $files . " (OFF) <br/>";
+                                $negativeanswer++;
+                        }
+                        }
+                }
+                        if ($negativeanswer == 0){
+                                echo "No! (OK) <br/>";
+                        }
+                } else {
+                        echo "The file $dirpath doesn't exists (OK) <br/>";
+                }
+        }
 //List all files in a directory.
-	public function ListFilesonDir ($dirpath){
-		
-		$listfile = scandir($dirpath);
-		foreach( $listfile as $file ){
-			$file_full_path = "$dirpath$file/";
-			if(is_dir($file_full_path) and $file != "." and $file != ".."){
-				echo '- ' . $file . '<br/>';
-			}	
-		}
-	}
+        public function ListFilesonDir ($dirpath){
+
+                $listfile = scandir($dirpath);
+                foreach( $listfile as $file ){
+                        $file_full_path = "$dirpath$file/";
+                        if(is_dir($file_full_path) and $file != "." and $file != ".."){
+                                echo '- ' . $file . '<br/>';
+                        }
+                }
+        }
+
 //##############
-	
 }
 
 echo "<script>
 function PrintPage(){
-	window.print();
+        window.print();
 }
 </script>
 ";
@@ -155,6 +158,8 @@ echo "<button onclick='PrintPage()'> Print the results </button> <br/> <br/>";
 
 //SPECIFIC VARIABLES
 
+$HTACCESSFILE = ".htaccess";
+$WPCONFIGFILE = "wp-config.php";
 $WPREADMEFILE = "readme.html";
 $WPINSTALLFILE = "wp-admin/install.php";
 $WPXMLRPCFILE = "xmlrpc.php";
@@ -184,7 +189,7 @@ $WPTHEMESTWENTYSIXTEENJS = "./wp-content/themes/twentysixteen/js";
 
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
 
-echo "The current version of the Wordpress is: "; 
+echo "The current version of the Wordpress is: ";
 bloginfo('version');
 echo ". Please, verify the latest version on the Wordpress website: <a href='https://wordpress.org/download/'> Click Here </a> <br/> <br/>";
 //#########################################################
@@ -198,12 +203,26 @@ $WPauditCheck->CheckPerm($CORRECTPERM, $WPREADMEFILE);
 
 //Verify if the permission of the file  "./wp-admin/install.php" allows to access the page by third party.
 
+echo "This file ($WPINSTALLFILE) is no longer necessary, because your WordPress already been installed. ";
 $WPauditCheck->CheckPerm($CORRECTPERM, $WPINSTALLFILE);
+
 //#########################################################
 
-//Verify if the permission of the file xmlrpc.php allows to access the page by third party.
+//Verify if the permission of the file "xmlrpc.php" allows to access the page by third party.
 
 $WPauditCheck->CheckPerm($CORRECTPERM, $WPXMLRPCFILE);
+//#########################################################
+
+// Verify if the permission of the file ".htaccess" allows to access the page by third party.
+
+$WPauditCheck->CheckPerm("400", $HTACCESSFILE);
+
+//#########################################################
+
+// Verify if the permission of the file "wp-config.php" allows to access the page by third party.
+
+$WPauditCheck->CheckPerm("400", $WPCONFIGFILE);
+
 //#########################################################
 
 //Verify if the blank index.html are in the directories to avoid directory browsing.
@@ -212,12 +231,12 @@ $indexes = array( $WPADMINJSDIR, $WPADMINCSSDIR, $WPADMINIMAGESDIR, $WPADMINFONT
 
 echo "<br/> <b> ### Browsing Directory ### </b> <br/>";
 foreach ($indexes as $dirs){
-	$currentdir = $dirs."index.html";
-	if(!file_exists($currentdir)){
-		echo "<br/>Create a index.hml file in the directory '".$dirs."'. (OFF)";
-	} else {
-		echo "<br/> The index.html file has already been created in the directory '". $dirs."'. (OK)";
-	}
+        $currentdir = $dirs."index.html";
+        if(!file_exists($currentdir)){
+                echo "<br/>Create a index.hml file in the directory '".$dirs."'. (OFF)";
+        } else {
+                echo "<br/> The index.html file has already been created in the directory '". $dirs."'. (OK)";
+        }
 }
 
 echo "<br/>";
@@ -226,11 +245,11 @@ echo "<br/>";
 
 //Check if the Wordpress uses SSL connection.
 echo "<br/> <b> It uses SSL Connection? </b> <br/>";
-	if (is_ssl()){
-		echo "Uses SSL connection (HTTPS). (OK)";
-	} else {
-		echo "Doesn't use SSL connection (HTTPS). (OFF)";
-	}
+        if (is_ssl()){
+                echo "Uses SSL connection (HTTPS). (OK)";
+        } else {
+                echo "Doesn't use SSL connection (HTTPS). (OFF)";
+        }
 
 //#########################################################
 
@@ -246,22 +265,22 @@ echo "<br/> <b> ### Installed Themes ### </b> <br/>";
 $WPauditCheck->ListFilesonDir($WPTHEMEDIR);
 //#########################################################
 
-//Verify if exists backup file "tar.gz" and ".zip" (etc) or backup of the database. 
+//Verify if exists backup file "tar.gz" and ".zip" (etc) or backup of the database.
 $filelistwebrootdir = scandir("./");
 echo "<br/> <b> Have compressed or SQL files in the main directory? </b> <br/> <br/>";
 $negativeanswer = 0;
 
 foreach( $filelistwebrootdir as $files ){
-	if(!is_dir($files) and $files != "." and $files != ".."){
-		$ext = pathinfo($files, PATHINFO_EXTENSION);
-		if($ext == "sql" or $ext == "zip" or $ext == "gz" or $ext == "bz2" or $ext == "tar" or $ext == "7z"){
-			echo $files ." (OFF) <br/>";
-			$negativeanswer++;
-		}
-	}
+        if(!is_dir($files) and $files != "." and $files != ".."){
+                $ext = pathinfo($files, PATHINFO_EXTENSION);
+                if($ext == "sql" or $ext == "zip" or $ext == "gz" or $ext == "bz2" or $ext == "tar" or $ext == "7z"){
+                        echo $files ." (OFF) <br/>";
+                        $negativeanswer++;
+                }
+        }
 }
 if ($negativeanswer == 0){
-	echo "No! (OK) <br/>";
+        echo "No! (OK) <br/>";
 }
 
 //#########################################################
@@ -361,7 +380,9 @@ echo "<br/> <b> Additional Recommendations: </b> <br/> <br/>";
 
 echo "You should create your own salt hash to customize the wp-config.php file, just access the following page: <a href='https://api.wordpress.org/secret-key/1.1/salt'> Click Here </a> <br/>";
 
-echo "<br/> Is recommended to access the following link to see the complete hardening checklist in the Wordpress: " . "<a href='https://www.owasp.org/index.php/OWASP_Wordpress_Security_Implementation_Guideline'> Click Here </a> <br/> <br/>";
+echo "<br/> Is recommended to access the following link to see the complete hardening checklist in the Wordpress: " . "<a href='https://www.owasp.org/index.php/OWASP_Wordpress_Security_Implementation_Guideline'> Click Here </a> <br/>";
+
+echo "<br/> Is recommended also to access the following link to see the complete hardening checklist in the Wordpress: " . "<a href='https://wordpress.org/about/security/'> Click Here </a> <br/> <br/>";
 
 //##### WORDPRESS ##### END
 
@@ -396,28 +417,28 @@ echo "<br/> <br/> <b> ### Installed Templates ### </b> <br/>";
 $JoomlaCheck->ListFilesonDir($JOOMLATEMPLATESDIR);
 //#########################################################
 
-//Verify if exists backup file "tar.gz" and ".zip" (etc) or backup of the database. 
+//Verify if exists backup file "tar.gz" and ".zip" (etc) or backup of the database.
 $filelistwebrootdir = scandir("./");
 echo "<br/> <b> Have compressed or SQL files in the main directory? </b> <br/> <br/>";
 $negativeanswer = 0;
 
 foreach( $filelistwebrootdir as $files ){
-	if(!is_dir($files) and $files != "." and $files != ".."){
-		$ext = pathinfo($files, PATHINFO_EXTENSION);
-		if($ext == "sql" or $ext == "zip" or $ext == "gz" or $ext == "bz2" or $ext == "tar" or $ext == "7z"){
-			echo $files ." (OFF) <br/>";
-			$negativeanswer++;
-		}
-	}
+        if(!is_dir($files) and $files != "." and $files != ".."){
+                $ext = pathinfo($files, PATHINFO_EXTENSION);
+                if($ext == "sql" or $ext == "zip" or $ext == "gz" or $ext == "bz2" or $ext == "tar" or $ext == "7z"){
+                        echo $files ." (OFF) <br/>";
+                        $negativeanswer++;
+                }
+        }
 }
 if ($negativeanswer == 0){
-	echo "No! (OK) <br/>";
+        echo "No! (OK) <br/>";
 }
 
 //#########################################################
 //##### JOOMLA ##### END
-	} else {
-	echo "<b> [WARNING] This script doesn't support this CMS installation or put this script in the web root directory of this web service/account. [WARNING] </b>";
+        } else {
+        echo "<b> [WARNING] This script doesn't support this CMS installation or put this script in the web root directory of this web service/account. [WARNING] </b>";
 }
 
 ?>
